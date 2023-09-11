f07bc46aea91a269a5bc55a46d4f6ddb3c55e9fc
kionBreadley01 <juansanchezjuan125@gmail.com>
2023-09-11 11:27:05 -0600

    02  comwnsando con javascript

---
+++ datos.js
@@ -0,0 +1,18 @@
+console.log('Ejecuntadose desde Node');
+// tipos de datos 
+console.log(12);
+console.log(typeof(12));
+console.log(typeof(12.24));
+console.log(true);
+console.log(typeof(true));
+console.log("este es un String");
+console.log(typeof("este es un String"));
+let variable;
+console.log(variable);
+console.log([]);
+console.log(typeof([]));
+console.log([1, 2, 3 ]);
+console.log({name})
+
+
+
