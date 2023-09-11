67949ac389e020f5def0ea7b429537f2cdd063f2
kionBreadley01 <juansanchezjuan125@gmail.com>
2023-09-11 11:41:51 -0600

    03 variablest

---
+++ variables.js
@@ -0,0 +1,22 @@
+// varian anteriores a ES& era A var 
+var variable;
+// de ES& adelnalte
+// const y let
+// const decalra constantes, no permitir reasignacion 
+// AL crearla debe inicializarae 
+const PI = 3.1416;
+// PI = 3,28;
+
+// ley decara variabless 
+//  permite reasignacion 
+//  de puede declara y luego imicializar
+// 35 dectecta  autokaticamnete el tipo de dato 
+let firstname;
+console.log('firsname');
+firstname = 'peter';
+console.log(firstname);
+console.log (typeof(firstname));
+firstname =18;
+console.log (firstname);
+console.log (typeof(firstname));
+
