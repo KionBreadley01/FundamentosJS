8345e5d342fd2fbf3cd183a9596abcf0cd8f8d51
kionBreadley01 <juansanchezjuan125@gmail.com>
2023-09-19 13:36:07 -0600

    09 ciclos

---
+++ ciclos.js
@@ -0,0 +1,40 @@
+// Ciclos en java script
+// for 
+for (let i = 0; 1 < 3; i++){
+
+    console.log('indice' + 1);
+}
+
+
+//While 
+let i = 0;
+while (i < 3) {
+    console.log('indice' + i);
+    i++;
+
+}
+
+// do While
+i = 0;
+do {
+    console.log('Indice' + i);
+    i++;
+} while (i === 0);
+
+const letras = 'adcd';
+const nemros = [3, 1, 5, 2, ];
+
+for(let i = 0; i < letras.length; i++){
+    console.log(letras[1]);
+}
+
+
+// for of 
+
+console.log('for of\n');
+for (const letra of letras){
+    console.log(letra);
+
+}
+
+
